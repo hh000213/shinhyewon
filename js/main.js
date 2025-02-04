2799cf292dab7e803c3052f6096896358b050dba
hh000213 <hh000213@naver.com>
2025-02-04 16:25:13 +0900

    third commit

--- js/main.js
+++ js/main.js
@@ -299,7 +299,7 @@ for (var i = 0; i < nameElmt.length; i++) {
 $('.banner img').click(function(e){
    e.preventDefault()
    let imageSrc = $(this).attr('src');
-   let fullImageSrc = `../${imageSrc}`
+   let fullImageSrc = `${imageSrc}`
    let idx = $(this).index()
    $('.popup .bnr').css({
       'background-image' : `url('${fullImageSrc}')`
